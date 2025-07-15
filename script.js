018c7eedac844f914288a45719e77617cbe9b8c3
AHMAD NADEEM <113020323+ahmadnadeem-12@users.noreply.github.com>
2025-07-15 14:09:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const display = document.querySelector("#inp")
+const button = document.querySelectorAll("button")
+
+console.log(button)
+let arraybutton = Array.from(button)
+let string="";
+console.log(arraybutton)
+arraybutton.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=="DEL")
+        {
+            string=string.substring(0,string.length-1)
+            display.value=string;
+        }
+        else if(e.target.innerHTML=="AC"){
+            string="";
+            display.value=string;
+        }
+        else if(e.target.innerHTML=="="){
+            string=eval(string).toString();
+            display.value=string;
+        }
+        else
+        {
+            string+=e.target.innerHTML;
+            display.value=string;
+            display.scrollLeft=display.scrollWidth;
+        }
+    })
+})
